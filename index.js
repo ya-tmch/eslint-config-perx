292ba43e35b4e958a0fc9cacc293016ebabddcbd
Artem Novolodskiy <ya.tmch@ya.ru>
2020-08-03 17:49:27 +0200

    Update

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 module.exports = {
-    rules: {
-
-    }
+  parser: 'babel-eslint',
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  plugins: ['react', 'react-hooks', 'prettier'],
+  settings: {
+    'import/core-modules': ['react'],
+  },
+  env: {
+    browser: true,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'react/no-danger': 0,
+    'import/no-unresolved': 0,
+    'react/sort-comp': 0,
+    'no-console': 0,
+    'react/destructuring-assignment': 0,
+    'react/jsx-props-no-spreading': 0,
+  },
 };
