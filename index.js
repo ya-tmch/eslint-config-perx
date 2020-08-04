5be9056ef40a2b19c50fb7bf8245d7eaf8dbfff1
Artem Novolodskiy <ya.tmch@ya.ru>
2020-08-04 09:38:33 +0200

    Update

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ module.exports = {
     'import/no-unresolved': 0,
     'react/sort-comp': 0,
     'no-console': 0,
+    'import/no-extraneous-dependencies': 0,
+    'lines-between-class-members': 0,
+    'prefer-destructuring': 0,
     'react/destructuring-assignment': 0,
     'react/jsx-props-no-spreading': 0
   }
